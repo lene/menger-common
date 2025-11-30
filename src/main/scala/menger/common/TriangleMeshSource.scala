d0332d020d0e27640dacfacc6e3b0a013b184dcc
Lene Preuss <lene.preuss@gmail.com>
2025-11-30 22:43:24 +0100

    Use curried functions for test assertions, remove redundant docstrings

--- src/main/scala/menger/common/TriangleMeshSource.scala
+++ src/main/scala/menger/common/TriangleMeshSource.scala
@@ -1,9 +1,4 @@
 package menger.common
 
-/** Trait for objects that can produce renderer-agnostic triangle mesh data.
-  *
-  * Implemented by geometry-producing types (Face, Cube, Sponge, etc.) to enable conversion to both
-  * LibGDX and OptiX representations.
-  */
 trait TriangleMeshSource:
   def toTriangleMesh: TriangleMeshData
